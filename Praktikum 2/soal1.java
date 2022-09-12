5eefaed54325c3349652ff93bb683fc5e77c1883
SultanShalahuddin <102792437+SultanShalahuddin@users.noreply.github.com>
2022-09-12 12:01:05 +0700

    Create soal1.java

---
+++ Praktikum 2/soal1.java
@@ -0,0 +1,14 @@
+public class soal1{
+ public static void main(String args[]) {
+ int num1 = 3;
+ int num2 = 2;
+ double ex = num1;
+
+ 
+ System.out.println("num1 + num2 = " + (num1 + num2) );
+ System.out.println("num1 - num2 = " + (num1 - num2) );
+ System.out.println("num1 * num2 = " + (num1 * num2) );
+ System.out.println("num1 / num2 = " + (ex / num2) );
+ System.out.println("num1 % num2 = " + (num1 % num2) );
+
+ }
